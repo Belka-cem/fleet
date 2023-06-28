8192e84cc6d1113d1363480080fd219ba7a4ae5d
Belkacem SAADI <sb@scepia.fr>
2023-06-28 13:42:09 +0200

    test register vihicle ok

--- features/scenarios/register_vehicle.feature
+++ features/scenarios/register_vehicle.feature
@@ -12,11 +12,11 @@ Feature: Vehicle Registration
     Given a fleet with ID "123"
     And a non-existing vehicle with ID "00-000-00"
     When I register the non-existing vehicle in the fleet
-    Then the registration should fail
+    Then the registration of non-existing vehicle should fail
 
-  
+  @prod
   Scenario: Register a vehicle in a non-existing fleet
     Given a non-existing fleet with ID "noFleet"
-    And a vehicle with ID "123456"
-    When I register the vehicle in the fleet
-    Then the registration should fail
+    And a existing vehicle with ID "123456"
+    When I register the vehicle in the non-existing fleet
+    Then the registration in non-existing fleet should fail
--- features/step_definitions/register_vehicle.ts
+++ features/step_definitions/register_vehicle.ts
@@ -9,6 +9,7 @@ let vehicleId: string;
 let result: string;
 
 
+//1
 Given('a fleet with ID {string}', function (id: string) {
   fleetId = id;
 });
@@ -31,10 +32,11 @@ Then('the vehicle should be added to the fleet', function () {
   }
 });
 
-
+//2
 Given('a non-existing vehicle with ID {string}', function (id:string) {
   vehicleId = id;
 });
+
 
 When('I register the non-existing vehicle in the fleet', async function () {
   
@@ -43,7 +45,36 @@ When('I register the non-existing vehicle in the fleet', async function () {
     } 
 });
 
-Then('the registration should fail', async function () {
+Then('the registration of non-existing vehicle should fail', async function () {
+  try {
+    await this.result();
+    // Si aucune erreur n'est levée, nous lançons une AssertionError
+    assert.fail('Expected an error to be thrown');
+  } catch (error) {
+    // Vérifier que l'erreur est bien de type Error
+    assert.ok(error instanceof Error);
+  }
+   
+});
+
+
+//3
+Given('a non-existing fleet with ID {string}', function (id:string) {
+  fleetId = id;
+});
+
+Given('a existing vehicle with ID {string}', function (id: string) {
+  vehicleId = id;
+});
+
+When('I register the vehicle in the non-existing fleet', async function () {
+  
+    this.result = async () => {
+      await registerVehicle(fleetId, vehicleId);
+    } 
+});
+
+Then('the registration in non-existing fleet should fail', async function () {
   try {
     await this.result();
     // Si aucune erreur n'est levée, nous lançons une AssertionError
@@ -53,3 +84,4 @@ Then('the registration should fail', async function () {
     assert.ok(error instanceof Error);
   }
 });
+
